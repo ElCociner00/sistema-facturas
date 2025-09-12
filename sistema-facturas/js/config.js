e911ec2ac90c3844614fa5596a8f3655e1bd91ae
ElCociner00 <santiagoelchameluco@gmail.com>
2025-09-12 15:50:20 -0500

    sistema-facturas Update

--- sistema-facturas/js/config.js
+++ sistema-facturas/js/config.js
@@ -1,5 +1,6 @@
 const CONFIG = {
-    SHEET_URL: '/api/fetchSheet',
+    // PROXY 100% FUNCIONAL - Sin funciones serverless
+    SHEET_URL: 'https://corsproxy.io/?' + encodeURIComponent('https://docs.google.com/spreadsheets/d/e/2PACX-1vRBrazhlZ91OI-rcjqCCefnYmrOKm-pnqVqTGYhl1r_VnjS3u1PzdhnMT2GEKo0QPMXxXY84hcb_Eno/pub?output=csv'),
     
     usuariosAutorizados: {
         'admin': 'admin123',
